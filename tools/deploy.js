57124ce4daed711296f4c873bcc3b0e2de59dfc1
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2016-07-22 15:34:23 +0300

    fix linting issues

--- tools/deploy.js
+++ tools/deploy.js
@@ -1,16 +1,14 @@
 import { scp } from 'scp2';
-import fs from 'fs';
-
 const { ENV_DEPLOY_USER, ENV_DEPLOY_PASSWORD } = process.env;
 
 scp('dist/', {
-    port: 22,
-    host: '192.241.157.86',
-    username: ENV_DEPLOY_USER,
-    password: ENV_DEPLOY_PASSWORD,
-    path: '/var/web/jscompress.com/'
-}, function(err) {
-    if(err) {
-        throw err;
-    }
-})
+	port: 22,
+	host: '192.241.157.86',
+	username: ENV_DEPLOY_USER,
+	password: ENV_DEPLOY_PASSWORD,
+	path: '/var/web/jscompress.com/'
+}, err => {
+	if (err) {
+		throw err;
+	}
+});
