11efd1c13cb6433a4ac9cc56e78f7486464cfb88
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2018-05-27 16:58:51 +0300

    refactor: Fix lint error

--- js/util/uglify-js-browser.js
+++ js/util/uglify-js-browser.js
@@ -1,2 +1,2 @@
-// eslint-disable-next-line import/no-unresolved
+// eslint-disable-next-line import/no-unresolved, import/extensions
 export default from 'exports-loader?UglifyJS!uglify-js/uglify-js-browser';
